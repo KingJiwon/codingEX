265ee9392d4fda3e939018a18d4fdff588d7c3b1
학교컴 지원 <rqpnx@lxdrd.onmicrosoft.com>
2022-03-04 17:35:34 +0900

    dd

--- codingEX/M7.1.cpp
+++ codingEX/M7.1.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 using namespace std;
-
 typedef struct
 {
-	int spec[4][3] =
-	{
-		{18, 80, 3000},
-		{17, 70, 2500},
-		{20, 90, 3500},
-		{22, 100, 5000}
-	};
-}Robo;
+	char* name;
+	int height;
+	int weight;
+	int hpower;
+} Robot;
 
-void Print(Robo* Robot)
+void Print(Robot* a)
 {
-	Robo r;
-
-	for (int i = 0; i < 4 ; i++)
-	{
-		string name = "";
-		switch (i)
-		{
-		case 0: name = "태권브이";
-			break;
-		case 1: name = "마징가";
-			break;
-		case 2: name = "메칸더브이";
-			break;
-		case 3: name = "그랜다이져";
-		}
-		
-		cout << "이름:" << name << "  " << "신장:" << r.spec[i][0] << "m" << "  " << "무게:" << r.spec[i][1] << "T" << "  " << "마력:" << r.spec[i][2] << endl;
-		
-		
-	}
+	cout << "이름: " << a->name << endl;
+	cout << "신장: " << a->height << endl;
+	cout << "무게: " << a->weight << endl;
+	cout << "마력: " << a->hpower << endl;
 }
+void Avg(Robot a[4])
+{
+	float hsum = 0;
+	float wsum = 0;
+	float psum = 0;
 
-
-
-
+	for (int i = 0; i < 4; i++)
+	{
+		hsum += a[i].height;
+		wsum += a[i].weight;
+		psum += a[i].hpower;
+	}
+	cout << "평균 신장: " << hsum/4 << "M" << endl;
+	cout << "평균 무게: " << wsum / 4 << "T" << endl;
+	cout << "평균 마력: " << psum / 4 << endl;
+}
 void main()
 {
-	Robo a;
-	Print(&a);
+	Robot r[4] =
+	{
+		{ "태권브이", 18, 80, 3000},
+		{ "마징가", 17, 70, 2500 },
+		{ "메칸더브이", 20, 90, 3500 },
+		{ "그랜다이져", 22, 100, 5000 },
+	};
+	Robot a{};
+	int count = sizeof(r) / sizeof(a);
+
+	for (int i = 0; i < count; i++)
+	{
+		Print(&r[i]);
+	}
+	Avg(r);
 }
